02ab704934c1fa82a53418e7940efbbefc6ea1d5
mohiniuops <63095966+mohiniuops@users.noreply.github.com>
2020-08-23 18:27:25 +0530

    Update sendmail.py
    Commented below lines
    
    s3 = boto3.client('s3')
    s3.download_file('email-reports-bucket', 'NotificationOPS/target/email.html', 'email.html')
    html = open("email.html")
    msg = MIMEText(html.read(), 'html')
    text = msg.as_string()
    
    s = smtplib.SMTP('email-smtp.us-west-2.amazonaws.com')
    s.connect('email-smtp.us-west-2.amazonaws.com',587)
    s.starttls()
    s.login(os.environ(SMTP_ACCESS),os.environ(SMTP_PASS))
    msg="From: mohinidevops@gmail.com \nTo: mohinidevops@gmail.com \nSubject: TestMessage \n\n Hello"
    
    s.sendmail('mohinidevops@gmail.com','mohinidevops@gmail.com',text)

--- sendmail.py
+++ sendmail.py
@@ -4,16 +4,3 @@ import boto3
 import os
 
 
-s3 = boto3.client('s3')
-s3.download_file('email-reports-bucket', 'NotificationOPS/target/email.html', 'email.html')
-html = open("email.html")
-msg = MIMEText(html.read(), 'html')
-text = msg.as_string()
-
-s = smtplib.SMTP('email-smtp.us-west-2.amazonaws.com')
-s.connect('email-smtp.us-west-2.amazonaws.com',587)
-s.starttls()
-s.login(os.environ(SMTP_ACCESS),os.environ(SMTP_PASS))
-msg="From: mohinidevops@gmail.com \nTo: mohinidevops@gmail.com \nSubject: TestMessage \n\n Hello"
-
-s.sendmail('mohinidevops@gmail.com','mohinidevops@gmail.com',text)
