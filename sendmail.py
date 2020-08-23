ad5edbeacbf8b2bbf4d6615de2fe43ba9a0f0538
mohiniuops <63095966+mohiniuops@users.noreply.github.com>
2020-08-23 19:04:40 +0530

    Update sendmail.py
    Removed 
    s = smtplib.SMTP('email-smtp.us-west-2.amazonaws.com')
    s.connect('email-smtp.us-west-2.amazonaws.com',587)
    s.starttls()
    s.login(os.environ['SMTP_ACCESS'],os.environ['SMTP_PASS'])
    msg="From: mohinidevops@gmail.com \nTo: mohinidevops@gmail.com \nSubject: TestMessage \n\n Hello"
    
    s.sendmail('mohinidevops@gmail.com','mohinidevops@gmail.com',text)

--- sendmail.py
+++ sendmail.py
@@ -10,12 +10,6 @@ html = open("email.html")
 msg = MIMEText(html.read(), 'html')
 text = msg.as_string()
 
-s = smtplib.SMTP('email-smtp.us-west-2.amazonaws.com')
-s.connect('email-smtp.us-west-2.amazonaws.com',587)
-s.starttls()
-s.login(os.environ['SMTP_ACCESS'],os.environ['SMTP_PASS'])
-msg="From: mohinidevops@gmail.com \nTo: mohinidevops@gmail.com \nSubject: TestMessage \n\n Hello"
 
-s.sendmail('mohinidevops@gmail.com','mohinidevops@gmail.com',text)
 
 
