b4599c72785f03ccdb33ae3a6cd03867d4091958
mohiniu <mohini4job@gmail.com>
2020-08-23 13:47:29 +0530

    Modified py

--- sendmail.py
+++ sendmail.py
@@ -1,6 +1,8 @@
 import smtplib
 from email.mime.text import MIMEText
 import boto3
+import os
+
 
 s3 = boto3.client('s3')
 s3.download_file('email-reports-bucket', 'NotificationOPS/target/email.html', 'email.html')
@@ -11,7 +13,7 @@ text = msg.as_string()
 s = smtplib.SMTP()
 s.connect('email-smtp.us-west-2.amazonaws.com',587)
 s.starttls()
-s.login('AKIAQUEGDMGOSVYWI2FM','BPuHsAM88e+o+B7BzoZQisqnqmm7TqytrYInq9fIGrQT')
+s.login(os.environ(SMTP_ACCESS),os.environ(SMTP_PASS))
 msg="From: mohinidevops@gmail.com \nTo: mohinidevops@gmail.com \nSubject: TestMessage \n\n Hello"
 
 s.sendmail('mohinidevops@gmail.com','mohinidevops@gmail.com',text)
